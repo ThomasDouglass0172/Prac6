8f7b732374e383487bb46d43004af614ac9957b5
ThomasDouglass0172 <100816798+ThomasDouglass0172@users.noreply.github.com>
2022-04-28 12:22:49 +0930

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "BitFlip.h"
+#include "BitFlipProb.h"
+#include "Rearrange.h"
+#include "Mutator.h"
+#include "Individual.h"
+#include <iostream> 
+
+using namespace std; 
+
+int main(void){
+
+
+return 0; 
+}
